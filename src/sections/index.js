95528084244aa1268a30dc547e8cc7930e84e93d
Auriol NGUELI <auriol.ngueli-eboue.ext@siemens.com>
2024-06-06 12:15:38 +0200

    Creating sections (initizlyzing the App)

---
+++ src/sections/index.js
@@ -0,0 +1,21 @@
+import Hero from "./Hero.jsx";
+import CustomSection from "./CustomSection.jsx";
+import Footer from "./Footer.jsx";
+import CustomerReviews from "./CustomerReviews.jsx";
+import PopularProducts from "./PopularProducts.jsx";
+import Services from "./Services.jsx";
+import SpecialOffer from "./SpecialOffer.jsx";
+import Subscribe from "./Subscribe.jsx";
+import SuperQuality from "./SuperQuality.jsx";
+
+export {
+    Hero,
+    CustomSection,
+    CustomerReviews,
+    Footer,
+    PopularProducts,
+    Services,
+    SpecialOffer,
+    Subscribe,
+    SuperQuality
+};
